f69d9ecbaf4f71379e7297d8c30b0587d713bb67
TYU-Zeng <tizeng@student.unimelb.edu.au>
2024-09-13 16:20:08 +1000

    update - sample demo

--- Cats_Catch_Mice/app/src/main/java/com/example/cats_catch_mice/ItemDetail.java
+++ Cats_Catch_Mice/app/src/main/java/com/example/cats_catch_mice/ItemDetail.java
@@ -3,6 +3,7 @@ package com.example.cats_catch_mice;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,19 +19,26 @@ public class ItemDetail extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_item_detail);
 
-        // 设置 window insets
+        // set window insets
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        // 查找按钮并设置点击事件监听器
-        Button button = findViewById(R.id.button_cancel);
-        button.setOnClickListener(v -> {
 
+        Button buttonCancel = findViewById(R.id.button_cancel);
+        buttonCancel.setOnClickListener(v -> {
             Intent intent = new Intent(this, MainActivity.class);
+
             startActivity(intent);
         });
+
+        Button buttonUse = findViewById(R.id.button_use);
+        buttonUse.setOnClickListener(v -> {
+            Toast.makeText(this, "Item used!", Toast.LENGTH_SHORT).show();
+        });
+
+
     }
 }
