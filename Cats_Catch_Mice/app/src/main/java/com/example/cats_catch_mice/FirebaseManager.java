09f943064c1aa672fe608cf3aaabcce588fbc9c2
brian <brianhsieh38@gmail.com>
2024-11-01 23:43:46 +1100

    Deleted unused code

--- Cats_Catch_Mice/app/src/main/java/com/example/cats_catch_mice/FirebaseManager.java
+++ Cats_Catch_Mice/app/src/main/java/com/example/cats_catch_mice/FirebaseManager.java
@@ -2,12 +2,8 @@ package com.example.cats_catch_mice;
 
 import static android.content.ContentValues.TAG;
 
-import android.os.Build;
 import android.util.Log;
 import android.util.Pair;
-import android.widget.Toast;
-
-import java.lang.reflect.Array;
 import java.security.SecureRandom;
 import androidx.annotation.NonNull;
 import androidx.lifecycle.LiveData;
@@ -35,38 +31,11 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingDeque;
-import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class FirebaseManager extends ViewModel {
 
-    /*
-    sync data to firebase
-    这里只需要写入数据的子节点 rooms的数据进入database
-    数据格式如下：
-
-    "rooms": {
-    "roomId12345": {
-      
-        "UUID12345":"owner": "UUID12345",
-      "members": { {
-          "lat": 37.7749,
-          "lng": -122.4194,
-          "item1": 0,
-          "item2": 1
-        },
-        "UUID67890": {
-          "lat": 37.7750,
-          "lng": -122.4200,
-          "item1": 2,
-          "item2": 3
-        }
-      }
-    }
-
-
-      */
     private static final int CORE_THREADS = 5;
     private static final int MAX_THREADS = 10;
     private static final int THREAD_LIFE = 30;
@@ -79,12 +48,10 @@ public class FirebaseManager extends ViewModel {
     private static final int INVISIBLE_TIMER = 20000;
 
     private final ThreadPoolExecutor executor;
-    private SecureRandom secureRandom;
     private FirebaseDatabase database;
     private String roomId;
     private String playerId;
     private boolean isOwner;
-    private String roomOwnerId;
 
     private double lastLat;
     private double lastLng;
@@ -105,7 +72,6 @@ public class FirebaseManager extends ViewModel {
                 Executors.defaultThreadFactory(),
                 new ThreadPoolExecutor.CallerRunsPolicy()
         );
-        secureRandom = new SecureRandom();
     }
 
     @Override
