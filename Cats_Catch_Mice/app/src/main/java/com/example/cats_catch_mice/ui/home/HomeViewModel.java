e3c2c7834358ad56a6cb42feb3a8518cbca5f820
brian <brianhsieh38@gmail.com>
2024-11-01 23:35:55 +1100

    Deleted unused code

--- Cats_Catch_Mice/app/src/main/java/com/example/cats_catch_mice/ui/home/HomeViewModel.java
+++ Cats_Catch_Mice/app/src/main/java/com/example/cats_catch_mice/ui/home/HomeViewModel.java
@@ -2,30 +2,11 @@ package com.example.cats_catch_mice.ui.home;
 
 import androidx.lifecycle.ViewModel;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
-import com.example.cats_catch_mice.Test;
-
 public class HomeViewModel extends ViewModel {
 
     public long catchButtonCooldownEndTime = 0L;
-//    private final MutableLiveData<String> mText;
-    private Test test;
 
     public HomeViewModel() {
-//        mText = new MutableLiveData<>();
-        //mText.setValue("This is home fragment");
     }
 
-    public void initTest(int a){
-        this.test = new Test(a);
-    }
-
-    public Test getTest(){
-        return this.test;
-    }
-
-//    public LiveData<String> getText() {
-//        return mText;
-//    }
 }
